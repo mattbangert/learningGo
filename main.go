ab0efb605f4f56ed85cc56ca913191d91aad6947
Matt Bangert <matt.bangert@smarsh.com>
2017-02-22 11:25:04 -0800

    Shorthand for declaring and assigning values.

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os")
+)
 
 
 func main() {
-	if len(os.Args) != 2 {
-		os.Exit(1)
-	}
+	power := 9000
 
-	fmt.Print("It's over", os.Args[1])
+	fmt.Printf("It's over %d\n", power)
 }
